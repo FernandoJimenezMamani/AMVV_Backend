6acef0f667159e700e3e4f109fef8c547b839d87
unknown <raymondverletzer@gmail.com>
2025-06-14 21:36:18 -0400

    Correccion de error ortografico

--- seeders/seed-lugares.js
+++ seeders/seed-lugares.js
@@ -7,8 +7,8 @@ const seedLugares = async () => {
 
     // Datos de lugares de ejemplo
     const lugares = [
-      { nombre: 'Coliceo Bolivar', longitud: -58.3816, latitud: -34.6037, eliminado: 0, direccion: 'Av. Principal 123' },
-      { nombre: 'Coliceo Coronilla', longitud: -58.4321, latitud: -34.5623, eliminado: 0, direccion: 'Calle Norte 456' },
+      { nombre: 'Coliseo Bolivar', longitud: -58.3816, latitud: -34.6037, eliminado: 0, direccion: 'Av. Principal 123' },
+      { nombre: 'Coliseo Coronilla', longitud: -58.4321, latitud: -34.5623, eliminado: 0, direccion: 'Calle Norte 456' },
       { nombre: 'Colegio San Marcos', longitud: -58.3987, latitud: -34.5892, eliminado: 0, direccion: 'Av. Sur 789' },
       { nombre: 'OTB Juan Vargas', longitud: -58.4723, latitud: -34.5211, eliminado: 0, direccion: 'Plaza Municipal 101' },
       { nombre: 'OTB Quiñones', longitud: -58.3524, latitud: -34.6345, eliminado: 0, direccion: 'Av. Deportes 202' },
